f0566dc82adafccdcc386a526055b1b37aad0a6c
Yanisley Mora Ritchie <ymritchie@gmail.com>
2019-07-06 16:16:01 -0300

    Tratamento padrao de erros com Alert

--- src/interceptors/error-interceptor.ts
+++ src/interceptors/error-interceptor.ts
@@ -2,12 +2,12 @@ import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HTTP_INTERCEPTORS
 import { Observable } from "rxjs/Rx";
 import { Injectable } from "@angular/core";
 import { StorageService } from "../services/storage.service";
+import { AlertController } from "ionic-angular";
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor{
     
-    constructor (public storage: StorageService){
-
+    constructor (public storage: StorageService, public alertController: AlertController){
     }
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
@@ -26,9 +26,14 @@ export class ErrorInterceptor implements HttpInterceptor{
             console.log(errorObj);
 
             switch(errorObj.status){
+                case 401:
+                    this.handle401();
+                    break;
                 case 403:
                     this.handle403();
                     break;
+                default:
+                    this.handleDefaultError(errorObj);
 
 
             }
@@ -40,6 +45,32 @@ export class ErrorInterceptor implements HttpInterceptor{
 
     handle403(){
         this.storage.setLocalUser(null);
+    }
+
+    handle401(){
+        let alter = this.alertController.create({
+            title: 'Erro 401: Falha de autenticação.',
+            message: 'Email ou senha incorretos.',
+            enableBackdropDismiss: false,
+            buttons: [
+                {text:  'Ok'}
+            ]
+        });
+
+        alter.present();
+    }
+
+    handleDefaultError(errorObj){
+        let alter = this.alertController.create({
+            title: 'Erro ' + errorObj.status + ': ' + errorObj.error,
+            message: errorObj.message,
+            enableBackdropDismiss: false,
+            buttons: [
+                {text:  'Ok'}
+            ]
+        });
+
+        alter.present();
     }
 }
 

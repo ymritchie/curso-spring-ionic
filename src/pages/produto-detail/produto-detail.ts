6efeebc6dcb198d66712448efdaa800d58b9d058
Yanisley Mora Ritchie <ymritchie@gmail.com>
2019-07-11 09:21:38 -0300

    Criando pagina de carrinho de compras

--- src/pages/produto-detail/produto-detail.ts
+++ src/pages/produto-detail/produto-detail.ts
@@ -3,6 +3,7 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { ProdutoDTO } from '../../models/produto.dto';
 import { ProdutoService } from '../../services/domain/produto.service';
 import { API_CONFIG } from '../../config/api.config';
+import { CartService } from '../../services/domain/cart.service';
 
 
 @IonicPage()
@@ -16,7 +17,8 @@ export class ProdutoDetailPage {
 
   constructor(public navCtrl: NavController, 
       public navParams: NavParams,
-      public produtoService: ProdutoService) {
+      public produtoService: ProdutoService,
+      public cartService: CartService) {
   }
 
   ionViewDidLoad() {
@@ -36,6 +38,11 @@ export class ProdutoDetailPage {
         this.produto.imageUrl = `${API_CONFIG.bucketBaseUrl}/prod${this.produto.id}.jpg`;
        },
       error => {});
+  }
+
+  addToCart(produto: ProdutoDTO){
+    this.cartService.addProduto(produto);
+    this.navCtrl.setRoot("CartPage");
   }
 
 }
---
+++ src/services/domain/cart.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from "@angular/core";
+import { StorageService } from "../storage.service";
+import { Cart } from "../../models/cart";
+import { ProdutoDTO } from "../../models/produto.dto";
+import { a } from "@angular/core/src/render3";
+import { ReturnStatement } from "@angular/compiler";
+
+@Injectable()
+export class CartService {
+
+    constructor(public storage: StorageService){
+
+    }
+
+    createOrClearCart(): Cart{
+        let cart: Cart = {items: []};
+        this.storage.setCart(cart);
+        return cart;
+    }
+
+    getCart() : Cart {
+        let cart: Cart = this.storage.getCart();
+
+        if (cart == null){
+            cart = this.createOrClearCart();
+        }
+
+        return cart;
+    }
+
+    addProduto(produto: ProdutoDTO) : Cart {
+        let cart = this.getCart();
+        let position = cart.items.findIndex(x => x.produto.id == produto.id);
+
+        if (position == -1){
+            cart.items.push({quantidade: 1, produto: produto});
+        }
+
+        this.storage.setCart(cart);
+        return cart;
+    }
+}

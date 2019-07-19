1d0165ead298a06561f94d5a20742917241adc04
Yanisley Mora Ritchie <ymritchie@gmail.com>
2019-07-19 17:24:45 -0300

    Componente Loading

--- src/pages/produtos/produtos.ts
+++ src/pages/produtos/produtos.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, LoadingController } from 'ionic-angular';
 import { ProdutoDTO } from '../../models/produto.dto';
 import { ProdutoService } from '../../services/domain/produto.service';
 import { API_CONFIG } from '../../config/api.config';
@@ -23,19 +23,21 @@ export class ProdutosPage {
   constructor(
     public navCtrl: NavController, 
     public navParams: NavParams,
-    public produtoService: ProdutoService) {
+    public produtoService: ProdutoService,
+    public loadingController: LoadingController) {
   }
 
   ionViewDidLoad() {
     let categoria_id = this.navParams.get("categoria_id");
-
+    let loader = this.presentLoading();
     this.produtoService.findByCategoria(categoria_id)
       .subscribe(response => {
         this.items = response['content'];
+        loader.dismiss();
         this.loadImageUrls();
       },
       error => {
-
+        loader.dismiss();
       });
   }
 
@@ -53,6 +55,14 @@ export class ProdutosPage {
 
   showDetail(produto_id: string){
     this.navCtrl.push("ProdutoDetailPage", {produto_id: produto_id});
+  }
+
+  presentLoading(){
+    let loader = this.loadingController.create({
+      content: "Aguarde..."
+    });
+    loader.present();
+    return loader;
   }
 
 }
